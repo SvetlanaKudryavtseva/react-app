2b1dbaaab2b508e40b75638bb394f4d486335ec0
Svetlana Kudryavtseva <springinmypalms@gmail.com>
2021-06-08 09:23:49 +0300

    activeClassName for links

--- 01-react-project/src/components/Navbar/Navbar.js
+++ 01-react-project/src/components/Navbar/Navbar.js
@@ -4,11 +4,11 @@ import styles from './Navbar.module.css';
 const Navbar = () => {
     return (
     <nav className={styles.nav}>
-        <div className={styles.item}><NavLink className={styles.link} to="/profile">Profile</NavLink></div>
-        <div className={styles.item}><NavLink className={styles.link} to="/dialogs">Messages</NavLink></div>
-        <div className={styles.item}><NavLink className={styles.link} to="/news">News</NavLink></div>
-        <div className={styles.item}><NavLink className={styles.link} to="/music">Music</NavLink></div>
-        <div className={styles.item}><NavLink className={styles.link} to="/settings">Settings</NavLink></div>
+        <div className={styles.item}><NavLink className={styles.link} activeClassName={styles.active} to="/profile">Profile</NavLink></div>
+        <div className={styles.item}><NavLink className={styles.link} activeClassName={styles.active} to="/dialogs">Messages</NavLink></div>
+        <div className={styles.item}><NavLink className={styles.link} activeClassName={styles.active} to="/news">News</NavLink></div>
+        <div className={styles.item}><NavLink className={styles.link} activeClassName={styles.active} to="/music">Music</NavLink></div>
+        <div className={styles.item}><NavLink className={styles.link} activeClassName={styles.active} to="/settings">Settings</NavLink></div>
     </nav>
     )
 }
