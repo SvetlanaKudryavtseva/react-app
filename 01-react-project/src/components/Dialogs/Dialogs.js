80f61465cece54d0bb2da4c7ab291b08e44f21fd
Svetlana Kudryavtseva <springinmypalms@gmail.com>
2021-06-09 20:29:44 +0300

    split UI and BLL data layers for dialogs and posts

--- 01-react-project/src/components/Dialogs/Dialogs.js
+++ 01-react-project/src/components/Dialogs/Dialogs.js
@@ -3,17 +3,29 @@ import Dialog from './Dialog/Dialog';
 import Message from './Message/Message';
 
 const Dialogs = () => {
+    let dialogData = [
+        {name: 'Olga', id: 1},
+        {name: 'Sveta', id: 2},
+        {name: 'Yulya', id: 3}
+    ]
+    let messageData = [
+        {text: 'Olga: Hi', id: 1},
+        {text: 'Sveta: Hi', id: 2},
+        {text: 'Yulya: Hi hi', id: 3}
+    ]
+    
+    
     return (
         <div className={styles.wrap}>
             <div className={styles.friend__list}>
-                <Dialog name="Olga" id="1"/>
-                <Dialog name="Sveta" id="2"/>
-                <Dialog name="Yulya" id="3"/>
+                <Dialog name={dialogData[0].name} id={dialogData[0].id}/>
+                <Dialog name={dialogData[1].name} id={dialogData[1].id}/>
+                <Dialog name={dialogData[2].name} id={dialogData[2].id}/>
             </div>
             <div className ={styles.messages__list}>
-                <Message text="Olga: Hi"/>
-                <Message text="Sveta: Hi"/>
-                <Message text="Yulya: Hi"/>
+                <Message text={messageData[0].text}/>
+                <Message text={messageData[1].text}/>
+                <Message text={messageData[2].text}/>
             </div>
         </div>
     )
--- 01-react-project/src/components/Profile/Posts/Posts.js
+++ 01-react-project/src/components/Profile/Posts/Posts.js
@@ -1,14 +1,19 @@
 import styles from './Posts.module.css'
 import Post from './Post/Post'
 const Posts = () => {
+    let postData = [
+        {id: 1, message: '1st message', likesCount: '23'},
+        {id: 2, message: '2nd message', likesCount: '17'},
+    ]
+    
     return (
     <div className={styles.wrap}>
         <textarea className={styles.textarea}>My Posts</textarea>
         <button className={styles.button}>Submit</button>
         <div className={styles.new_post}></div>
         <div className={styles.list}>
-            <Post message='1st message' likesCount='23'/>
-            <Post message='2nd message' likesCount='7'/>
+            <Post message={postData[0].message} likesCount={postData[0].likesCount}/>
+            <Post message={postData[1].message} likesCount={postData[1].likesCount}/>
         </div>
     </div>
     )
