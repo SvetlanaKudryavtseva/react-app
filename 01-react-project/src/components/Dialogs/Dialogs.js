2c51519d51f0a35aca6a89e201bf5794d59c9d66
Svetlana Kudryavtseva <springinmypalms@gmail.com>
2021-06-09 08:55:04 +0300

    dialogs component layout creation

--- 01-react-project/src/components/Dialogs/Dialogs.js
+++ 01-react-project/src/components/Dialogs/Dialogs.js
@@ -1,8 +1,16 @@
 import styles from './Dialogs.module.css';
+import Messages from './Messages/Messages';
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={styles.wrap}>
+            <div className={styles.list}>
+                <div className={styles.item + ' ' + styles.active}>Ira</div>
+                <div className={styles.item}>Sveta</div>
+                <div className={styles.item}>Dima</div>
+                <div className={styles.item}>Sasha</div>
+                <div className={styles.item}>Lena</div>
+            </div>
+            <Messages />
         </div>
     )
 }
