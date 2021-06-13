698a3ab2a1c9c8b018f34dd27660bd61862fe6e7
Svetlana Kudryavtseva <springinmypalms@gmail.com>
2021-06-13 10:08:28 +0300

    method map for dialogs and messages

--- 01-react-project/src/components/Dialogs/Dialogs.js
+++ 01-react-project/src/components/Dialogs/Dialogs.js
@@ -3,33 +3,25 @@ import Dialog from './Dialog/Dialog';
 import Message from './Message/Message';
 
 const Dialogs = () => {
-    let dialogData = [
+    let dialogs = [
         {name: 'Olga', id: 1},
         {name: 'Sveta', id: 2},
         {name: 'Yulya', id: 3}
     ]
-    let dialogItems = [
-        <Dialog name={dialogData[0].name} id={dialogData[0].id}/>,
-        <Dialog name={dialogData[1].name} id={dialogData[1].id}/>,
-        <Dialog name={dialogData[2].name} id={dialogData[2].id}/>
-    ]
-    let messageData = [
+    let dialogItems = dialogs.map ( d => <Dialog name={d.name} id={d.id}/> )
+    let messages = [
         {text: 'Olga: Hi', id: 1},
         {text: 'Sveta: Hi', id: 2},
         {text: 'Yulya: Hi hi', id: 3}
     ]
-
-    
-    
+    let messageItems = messages.map ( m =>  <Message text={m.text}/>)
     return (
         <div className={styles.wrap}>
             <div className={styles.friend__list}>
                 {dialogItems}
             </div>
             <div className ={styles.messages__list}>
-                <Message text={messageData[0].text}/>
-                <Message text={messageData[1].text}/>
-                <Message text={messageData[2].text}/>
+                { messageItems }
             </div>
         </div>
     )
